59979698288a566f776aa6d9ded6131d225f36b6
kalvin66 <kalvinnaro@gmail.com>
2024-04-24 22:39:30 +0700

    ganti

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('hello', function () {
+    return 'Hello World';
 });
+
+
+Route::get('/world', function () {
+    return 'world';
+});
+
+
+
+Route::get('/index', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 3312201011 <br> Nama : Kalvin Sebastian';
+});
+
+
+
+
+Route::get('/user/{name}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+
+
+Route::get('/user/profile', function () {
+    return "Nama Saya $name";
+})->name('profile');
+
+
+Route::get('/routeprofile', function () {
+    return to_route('profile', ['name' => 'kalvin']);
+});
+
+
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
